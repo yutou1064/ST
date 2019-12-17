ee1be793f2355ea0e51d1f545baadb806cfbeb69
喻通 <yutong@formssi.com>
2019-12-17 10:33:35 +0800

    add test

--- src/test/java/com/demo/StApplicationTests.java
+++ src/test/java/com/demo/StApplicationTests.java
@@ -2,12 +2,20 @@ package com.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootTest
-class StApplicationTests {
+public class StApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
+	@Test
+	@RequestMapping("/test")
+	public String test() {
+		return "test ok!";
+	}
+	
 }
