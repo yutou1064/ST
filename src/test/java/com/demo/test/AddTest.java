6f24b467dec496080f59ef9f716d6fc359257d05
喻通 <yutong@formssi.com>
2019-12-18 17:06:31 +0800

    add wrong test

---
+++ src/test/java/com/demo/test/AddTest.java
@@ -0,0 +1,21 @@
+package com.demo.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AddTest {
+
+	@Test
+	public void tests() {
+		int i = 1/0;
+		//int i = 1;
+		Assert.assertEquals(1,i);
+		System.out.println("aaa");
+	}
+	
+}
