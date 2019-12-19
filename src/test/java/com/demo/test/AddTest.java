58682777137b13ff783adc40d1972777464819b4
喻通 <yutong@formssi.com>
2019-12-19 10:33:28 +0800

    modify the add test

--- src/test/java/com/demo/test/AddTest.java
+++ src/test/java/com/demo/test/AddTest.java
@@ -11,11 +11,35 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class AddTest {
 
 	@Test
-	public void tests() {
+	public void tests1() {
 		//int i = 2;
 		int i = 1;
 		Assert.assertEquals(1,i);
-		System.out.println("aaa");
+		System.out.println("eee");
+	}
+	
+	@Test
+	public void tests2() {
+		//int i = 2;
+		int i = 1;
+		Assert.assertEquals(1,i);
+		System.out.println("fff");
+	}
+	
+	@Test
+	public void tests3() {
+		//int i = 2;
+		int i = 1;
+		Assert.assertEquals(1,i);
+		System.out.println("ggg");
+	}
+	
+	@Test
+	public void tests4() {
+		//int i = 2;
+		int i = 1;
+		Assert.assertEquals(1,i);
+		System.out.println("hhh");
 	}
 	
 }
