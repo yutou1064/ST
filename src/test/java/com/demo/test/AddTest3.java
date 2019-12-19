636ba4da5d45a7e564878d1d17a0d8ffa235bbca
喻通 <yutong@formssi.com>
2019-12-19 09:26:19 +0800

    add more test

---
+++ src/test/java/com/demo/test/AddTest3.java
@@ -0,0 +1,22 @@
+package com.demo.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest
+@PowerMockIgnore({"javax.management.*"})
+public class AddTest3 {
+
+	@Test
+	public void addTest1() {
+		System.out.println("1");
+		Assert.assertEquals(1, 1);
+	}
+	
+}
