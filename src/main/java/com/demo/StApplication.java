b76999d71010636960e40ff3b68109e6a703202b
FormS <FormS@MSI>
2020-07-31 15:49:25 +0800

    add org.apache.maven.plugins

--- src/main/java/com/demo/StApplication.java
+++ src/main/java/com/demo/StApplication.java
@@ -2,13 +2,18 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class StApplication {
+public class StApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StApplication.class);
+	}
 }
